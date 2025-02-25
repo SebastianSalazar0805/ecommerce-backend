3abaaa2b1b6fcc379e367e9d051bb9356ba8f304
Sebastian Salazar <jsalazar853@unab.edu.co>
2025-02-25 15:28:00 -0500

    ruta basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    echo "LISTADO DE PRODUCTOS DEL ECOMERCE";
+});
+
+Route::get('/products/create', function () {
+    echo "FORMULARIO PARA CREAR UN PRODUCTO:";
+});
+
+Route::get('/products/{name}', function ($name) {
+    echo "EL PRODUCTO ES: $name";
+});
+
+Route::get('/products/{name}/{categoria?}', function ($name, $categoria = null) {
+    if ($categoria == null){
+        echo "EK PRODUCTO ES: $name";
+    }else{
+        
+        echo "El producto es $name, y su categoria es $categoria";
+    }
+});
